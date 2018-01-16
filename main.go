1c2e571ce4175b2a76906f59aaac33fb83c84ee8
Gogs <gogs@fake.local>
2018-01-16 09:25:58 +0700

    delete static

--- main.go
+++ main.go
@@ -16,11 +16,11 @@ func main() {
 	router := gin.New()
 	//static
 	router.StaticFS("/static", http.Dir("./static"))
-	router.StaticFS("/app", http.Dir("./app"))
-	router.StaticFS("/feedback", http.Dir("./device")).Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Content-Type", "text/html")
-		c.Next()
-	})
+	// router.StaticFS("/app", http.Dir("./app"))
+	// router.StaticFS("/feedback", http.Dir("./device")).Use(func(c *gin.Context) {
+	// 	c.Writer.Header().Set("Content-Type", "text/html")
+	// 	c.Next()
+	// })
 	router.Use(gin.Logger(), middleware.Recovery(), middleware.AddHeader())
 	//api
 	rootAPI := router.Group("/api")
