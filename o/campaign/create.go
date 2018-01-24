7c7f3889b6441921b3e04417ab3c294f183da821
Gogs <gogs@fake.local>
2018-01-24 09:42:07 +0700

    fix error msg campaign

--- o/campaign/create.go
+++ o/campaign/create.go
@@ -13,9 +13,13 @@ func (c *Campaign) Create() error {
 	rest.AssertNil(rest.Validate(c), survey.CheckExistSurveys(c.Surveys))
 	var campaigns, err = GetCampaignByDevices(c.Devices, c.Start)
 	rest.AssertNil(err)
-	for _, item := range campaigns {
-		errStr := fmt.Sprintf("Thiết bị %s không thể áp dụng vì đang được áp dụng trong chiến dịch %s", item.Device, item.Name)
-		return rest.BadRequest(errStr)
+	for _, campaign := range campaigns {
+		for _, device := range c.Devices {
+			if device == campaign.Device {
+				errStr := fmt.Sprintf("Thiết bị %s không thể áp dụng vì đang được áp dụng trong chiến dịch %s", device, campaign.Name)
+				return rest.BadRequest(errStr)
+			}
+		}
 	}
 	return CampaignTable.Create(c)
 }
